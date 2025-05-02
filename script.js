fec9978e4d0e1e794f6967e8ce3b1d736bfb7575
javily123 <deleonjavi07@gmail.com>
2025-05-01 18:17:08 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,8 @@
     checkVisibility();
   });
 
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+}
